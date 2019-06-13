b3ae8d1ef2400ee9717af843b6044c854f2896a9
Emerson Demetrio <emer.demetrio@gmail.com>
2019-06-12 23:57:36 -0300

    first commit

---
+++ game.js
@@ -0,0 +1,116 @@
+let matches = [];
+
+const playerHasWin = () => matches.length === keyWords.length;
+const showYouWin = () => document.getElementById('youwin').classList = 'show alert-success';
+const showYouLose = () => document.getElementById('youlose').classList = 'show alert-danger';
+const showTryAgain = () => document.getElementById('tryagain').classList = 'btn btn-primary';
+
+const table = document.getElementById('table');
+
+const getMatches = c => {
+	return matches.includes(c) ? c : '';
+};
+
+const updateView = () => {
+	let content = '<tr>';
+	let control = 0;
+
+	keyWords.forEach((k,
+		index) => {
+		content += `<td style="width: 100px; height: 40px">${getMatches(k)}</td>`;
+		control++;
+		if (control > 10) {
+			control = 0;
+			content += '</tr>';
+			if (index + 1 < keyWords.length) {
+				content += '<tr>';
+			}
+		}
+	});
+
+	table.innerHTML = content;
+};
+
+updateView();
+
+const isAMach = word => {
+	if (!matches.includes(word) && keyWords.includes(word)) {
+		matches.push(word);
+		return true;
+	}
+	return false;
+};
+
+const input = document.getElementById('input');
+const disaInput = () => input.setAttribute('disabled', 'disabled');
+const disableStart = () => document.getElementById('start').setAttribute('disabled', 'disabled');
+const enableInput = () => input.removeAttribute('disabled');
+const resetInput = () => input.value = '';
+const timer = document.getElementById('timer');
+
+const informMatches = () => {
+	const d = document.getElementById('matches');
+	d.innerHTML = `Matches: ${matches.length} / ${keyWords.length}`;
+};
+
+let from = 5 * 60;
+let timerInterval = null;
+
+const stopTimer = hasWin => {
+	hasWin ? showYouWin() : showYouLose();
+	clearInterval(timerInterval);
+};
+
+const decreaseTime = () => {
+	from = from - 1;
+	const date = new Date(null);
+	date.setSeconds(from);
+	var timeString = date.toISOString().substr(11, 8);
+
+	timer.innerHTML = timeString;
+
+	if (from === 0) {
+		disaInput();
+		showTryAgain();
+		stopTimer(false);
+		from = 60;
+	}
+};
+
+const startTimer = () => {
+	timerInterval = setInterval(decreaseTime, 1000);
+};
+
+const start = () => {
+	startTimer();
+	enableInput();
+	input.focus();
+	disableStart();
+};
+
+const receiveWord = value => {
+	if (isAMach(value)) {
+		informMatches();
+		updateView();
+		resetInput();
+	}
+
+	if (playerHasWin()) {
+		input.removeEventListener('keyup', receiveWord);
+		disaInput();
+		showYouWin();
+		showTryAgain();
+		stopTimer(true);
+	}
+};
+
+input
+	.addEventListener('keyup', evt => receiveWord(evt.target.value || ''));
+
+document
+	.getElementById('tryagain')
+	.addEventListener('click', () => window.location.reload());
+
+document
+	.getElementById('start')
+	.addEventListener('click', () => start());
